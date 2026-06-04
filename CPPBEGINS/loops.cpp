af7bc6f926062ee31e181089fda2d23b95beffa8
goofy-coder-1 <ujjwolrana2332@gmail.com>
2026-06-04 14:32:54 +0545

    hello Breezy

--- CPPBEGINS/loops.cpp
+++ CPPBEGINS/loops.cpp
@@ -2,15 +2,76 @@
 using namespace std;
 
 int main() {
-   for (int i = 0; i < 5; i++)
-   {
-    for (int j = 0; j < 5; j++)
-    {
-        cout << "Ujjwal ";
-    }
-    cout << "\n";
-   }
+//    for (int i = 0; i < 5; i++)
+//    {
+//     for (int j = 0; j < 5; j++)
+//     {
+//         cout << "Silencer ";
+//     }
+//     cout << "\n";
+//    }
    // Breezy dai ko code
+
+   // for the pattern
+   // 1
+   // 1 2
+   // 1 2 3
+   // 1 2 3 4
+   // 1 2 3 4 5
+//    for (int i = 1; i <= 5; i++)
+//    {
+//     for (int j = 1; j <= i; j++)
+//     {
+//         cout << j << " ";
+//     }
+//     cout << '\n';
+//    }
+   
+   // for pattern
+   // 1 2 3 4 5
+   // 1 2 3 4
+   // 1 2 3
+   // 1 2
+   // 1
+   for (int i = 1; i <= 5; i++)
+   {
+     for (int j = i; j >= i; j--)
+     {
+        cout << j << " ";
+     }
+     cout << "\n";
+   }
+   
+   // for pattern 
+   // *
+   // * *
+   // * * *
+   // * * * *
+   // * * * * *
+//     for (int i = 1; i <= 5; i++)
+//    {
+//     for (int j = 1; j <= i; j++)
+//     {
+//         cout << "* ";
+//     }
+//     cout << '\n';
+//    }
+
+    // for pattern
+    // 5
+    // 5 5
+    // 5 5 5
+    // 5 5 5 5
+    // 5 5 5 5 5
+//        for (int i = 1; i <= 5; i++)
+//    {
+//     for (int j = 1; j <= i; j++)
+//     {
+//         cout << "5 ";
+//     }
+//     cout << '\n';
+//    }
    
    return 0;
 }
+
