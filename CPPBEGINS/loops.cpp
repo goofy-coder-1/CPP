ded7519e71c9ddbab71efd11e2e73086fc6d72c6
goofy-coder-1 <ujjwolrana2332@gmail.com>
2026-06-04 14:37:32 +0545

    Homework hera hai rajiv

--- CPPBEGINS/loops.cpp
+++ CPPBEGINS/loops.cpp
@@ -33,14 +33,14 @@ int main() {
    // 1 2 3
    // 1 2
    // 1
-   for (int i = 1; i <= 5; i++)
-   {
-     for (int j = i; j >= i; j--)
-     {
-        cout << j << " ";
-     }
-     cout << "\n";
-   }
+//    for (int i = 5; i >= 1; i--)
+//    {
+//      for (int j = 1; j <= i; j++)
+//      {
+//         cout << j << " ";
+//      }
+//      cout << "\n";
+//    }
    
    // for pattern 
    // *
