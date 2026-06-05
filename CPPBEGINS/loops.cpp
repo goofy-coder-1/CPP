a057132373760659e74a226dbee70a13589369b1
goofy-coder-1 <ujjwolrana2332@gmail.com>
2026-06-05 06:44:20 +0545

    commit of the day

--- CPPBEGINS/loops.cpp
+++ CPPBEGINS/loops.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-//    for (int i = 0; i < 5; i++)
-//    {
-//     for (int j = 0; j < 5; j++)
-//     {
-//         cout << "Silencer ";
-//     }
-//     cout << "\n";
-//    }
+   for (int i = 0; i < 5; i++)
+   {
+    for (int j = 0; j < 5; j++)
+    {
+        cout << "Silencer ";
+    }
+    cout << "\n";
+   }
    // Breezy dai ko code
 
    // for the pattern
@@ -18,14 +18,14 @@ int main() {
    // 1 2 3
    // 1 2 3 4
    // 1 2 3 4 5
-//    for (int i = 1; i <= 5; i++)
-//    {
-//     for (int j = 1; j <= i; j++)
-//     {
-//         cout << j << " ";
-//     }
-//     cout << '\n';
-//    }
+   for (int i = 1; i <= 5; i++)
+   {
+    for (int j = 1; j <= i; j++)
+    {
+        cout << j << " ";
+    }
+    cout << '\n';
+   }
    
    // for pattern
    // 1 2 3 4 5
@@ -33,14 +33,14 @@ int main() {
    // 1 2 3
    // 1 2
    // 1
-//    for (int i = 5; i >= 1; i--)
-//    {
-//      for (int j = 1; j <= i; j++)
-//      {
-//         cout << j << " ";
-//      }
-//      cout << "\n";
-//    }
+   for (int i = 5; i >= 1; i--)
+   {
+     for (int j = 1; j <= i; j++)
+     {
+        cout << j << " ";
+     }
+     cout << "\n";
+   }
    
    // for pattern 
    // *
@@ -48,29 +48,29 @@ int main() {
    // * * *
    // * * * *
    // * * * * *
-//     for (int i = 1; i <= 5; i++)
-//    {
-//     for (int j = 1; j <= i; j++)
-//     {
-//         cout << "* ";
-//     }
-//     cout << '\n';
-//    }
+    for (int i = 1; i <= 5; i++)
+   {
+    for (int j = 1; j <= i; j++)
+    {
+        cout << "* ";
+    }
+    cout << '\n';
+   }
 
-    // for pattern
-    // 5
-    // 5 5
-    // 5 5 5
-    // 5 5 5 5
-    // 5 5 5 5 5
-//        for (int i = 1; i <= 5; i++)
-//    {
-//     for (int j = 1; j <= i; j++)
-//     {
-//         cout << "5 ";
-//     }
-//     cout << '\n';
-//    }
+   //  for pattern
+   //  5
+   //  5 5
+   //  5 5 5
+   //  5 5 5 5
+   //  5 5 5 5 5
+       for (int i = 1; i <= 5; i++)
+   {
+    for (int j = 1; j <= i; j++)
+    {
+        cout << "5 ";
+    }
+    cout << '\n';
+   }
    
    return 0;
 }
