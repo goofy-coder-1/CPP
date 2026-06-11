c0684552d2a47894b2ba1ab5d6a0f4d6f9f18bb9
goofy-coder-1 <ujjwolrana2332@gmail.com>
2026-06-11 15:36:35 +0545

    small commit

---
+++ CPPBEGINS/OOP/pin.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Pin {
+private:
+    int pin; 
+    
+public:
+    int account;
+    int age;
+
+   
+    void setPin(int p) {
+        pin = p;
+    }
+
+   
+    int getPin() {
+        return pin;
+    }
+};
+
+int main() {
+    Pin firstguy;
+    
+    
+    firstguy.setPin(1234); 
+    
+    firstguy.account = 321456;
+    firstguy.age = 20;
+    
+    cout << "Pin is: " << firstguy.getPin() << endl;
+    cout << "Age is: " << firstguy.age << endl;
+    
+    return 0;
+}
