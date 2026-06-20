33655ed03f63e7dacd81a64a4a07d622a583ac34
goofy-coder-1 <ujjwolrana2332@gmail.com>
2026-06-20 18:58:06 +0545

    wassup

--- CPPBEGINS/Inheritance/inheritance.cpp
+++ CPPBEGINS/Inheritance/inheritance.cpp
@@ -15,7 +15,7 @@ class MyChild: public MyClass {
 class MyGrandChild: public MyChild {};
 
 int main() {
-    MyGrandChild myObj; //creating 
+    MyGrandChild myObj; //creating object
     myObj.myFunction(); // calling function
     return 0;
 }
