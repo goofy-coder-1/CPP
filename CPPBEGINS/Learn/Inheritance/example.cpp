72d5b478f772a972f989fd2bacc52fab53fe12d0
goofy-coder-1 <ujjwolrana2332@gmail.com>
2026-06-26 11:47:09 +0545

    learning cpp

--- CPPBEGINS/Learn/Inheritance/example.cpp
+++ CPPBEGINS/Learn/Inheritance/example.cpp
@@ -5,4 +5,24 @@ using namespace std;
 class thermoStat {
     private:
     int targetTemperature;
+
+    public:
+    thermoStat(int initialTemp){
+        targetTemperature = initialTemp;
+    }
+
+    int getTemperature() const{
+        return targetTemperature;
+    }
+
+    void setTemperature(int newTemp) {
+        if (newTemp >= 50 && newTemp <= 90)
+        {
+            targetTemperature = newTemp;
+            cout << "Temperature set to " << targetTemperature << "F \n";
+        } else {
+            cout << newTemp << "is outside of safe operating limit";
+        }
+        
+    }
 };
